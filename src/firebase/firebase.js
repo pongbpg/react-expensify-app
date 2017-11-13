3b22c18a23ecc6585693304dcf65b06859081d8c
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-11-13 18:16:32 +0700

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,15 +4,16 @@ const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId:process.env.FIREBASE_PROJECT_ID,
+    projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // //child_removed snapshot.key,snapshot.val()
 // database.ref('expenses').on('child_removed', (snapshot) => {
