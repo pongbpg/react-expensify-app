14929bbbf5e9e0fcc899583e5420a2ec4e11bc47
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-11-14 14:39:14 +0700

    Change name to Expensify

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,8 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1 className="box-layout__title">Authentication App</h1>
-            <p>The new way to solve Identity.</p>
+            <h1 className="box-layout__title">Expensify App</h1>
+            <p>It's time to get your expenses under control.</p>
             <button className="button" onClick={startLogin}>Login with Google</button>
         </div>
 
